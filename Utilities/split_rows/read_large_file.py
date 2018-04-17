def0436055baf1de482fdd49a026f5d912a4ecd9
Anthony <aridding18@gmail.com>
2018-04-17 08:33:38 -0400

    Update read_large_file.py

--- Utilities/split_rows/read_large_file.py
+++ Utilities/split_rows/read_large_file.py
@@ -1,8 +1,20 @@
 import pandas as pd
 
-raw_data = pd.read_csv(r"C:\Users\aridding\Documents\Code Utilities\LargeFIleSplit\971051_74004_DealerClaimsDetails_1.csv", chunksize=100000, low_memory=False)
+#   User Defined Paths - placeholder values
+SRC_PATH = r"C:\\Users\\<path_to_large_file\\"
+SRC_FILE = "LARGE_FILE_GOES_HERE.csv"
+DST_PATH = r"C:\\Users\\aridding\\Documents\\Code Utilities\\LargeFIleSplit\\"
+DST_FILE = "FILE_NAME_OUTPUT.csv"
+
+
+#   Read the large file in via 'chunks' - set to 100k rows. 
+raw_data = pd.read_csv(SRC_PATH + SRC_FILE, chunksize=100000, low_memory=False)
+
+#   Value to append to the filename
 i = 0
+
+#   For each chunk - write to a new CSV. 
 for chunk in raw_data:
     i = i + 1
-    chunk.to_csv(r"C:\Users\aridding\Documents\Code Utilities\LargeFIleSplit\ClaimsByDealers_Core_" + str(i) + ".csv")
+    chunk.to_csv(\ClaimsByDealers_Core_" + str(i) + ".csv")
  
