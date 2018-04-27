4564e26a514a9bffa4317793ce6fe8c9431bfe2c
Anthony <aridding18@gmail.com>
2018-04-27 13:33:12 -0400

    Add files via upload

---
+++ BA/monthly_script/script_1_program_table.py
@@ -0,0 +1,73 @@
+#   Using the RAW "LAST DATA PULL" - Groupby Method
+
+#   Import the proper packages
+import pandas as pd
+
+
+#   Set up the source file path and name
+file_path = r"C:\\Users\\aridding\\Documents\\BA Related\\Monthly\\Monthly Report Development\\"
+_name = "Last Raw Data Pull-AR.csv"
+
+#   Read in the raw file - Check for dataset length (data validation)
+raw_all_years = pd.read_csv(file_path + _name, low_memory = False, encoding='latin-1', index_col=False)
+
+
+#   Split the dataset into 2016 and 2018 years
+all_years_programs_models_2016 = raw_all_years[raw_all_years["sale.year"] == 2016]
+all_years_programs_models_2018 = raw_all_years[raw_all_years["sale.year"] == 2018]
+
+
+#   Print descriptions - COUNT IS NOT ACCURATE FOR 2018 - SUM QUANTITY
+claim_count_2018 = all_years_programs_models_2018.groupby(["program.type"])["quantity"].sum()
+claim_count_2016 = all_years_programs_models_2016.groupby(["program.type"])["quantity"].sum()
+
+
+#   Claim Count - Breakdown by program - DataFrame. 
+df_claim_count_2016 = pd.DataFrame(claim_count_2016)
+df_claim_count_2018 = pd.DataFrame(claim_count_2018)
+
+
+#   Merge the dataframes to compare across years
+df_merge = pd.concat([df_claim_count_2016, df_claim_count_2018], axis=1)
+
+
+#   Rename the columns to be able to divide properly
+df_merge.columns = ["2016 Claim Count", "2018 Claim Count"]
+
+#   Percentage Change Calculation
+df_merge["Percentage Change"] = (df_merge["2018 Claim Count"]/df_merge["2016 Claim Count"] *100)- 100
+df_merge["Percentage Change"] = df_merge["Percentage Change"].round(2)
+
+
+#   Print descriptions - COUNT IS NOT ACCURATE FOR 2018 - SUM QUANTITY
+claim_amount_2018 = all_years_programs_models_2018.groupby(["program.type"])["claim.amount"].sum()
+claim_amount_2016 = all_years_programs_models_2016.groupby(["program.type"])["claim.amount"].sum()
+
+
+#   Claim Count - Breakdown by program - DataFrame. 
+df_claim_amount_2016 = pd.DataFrame(claim_amount_2016)
+df_claim_amount_2018 = pd.DataFrame(claim_amount_2018)
+
+
+#   Merge the dataframes to compare across years
+df_merge_amount = pd.concat([df_claim_amount_2016, df_claim_amount_2018], axis=1)
+
+#   Rename the columns to be able to divide properly
+df_merge_amount.columns = ["2016 Claim Amount", "2018 Claim Amount"]
+
+#   Merge the Count and Amount DFs
+count_amount_df = pd.concat([df_merge, df_merge_amount], 1)
+
+#   Percentage Change Calculation
+count_amount_df["Percentage Change - Amount"] = (count_amount_df["2018 Claim Amount"]/count_amount_df["2016 Claim Amount"] *100)- 100
+count_amount_df["Percentage Change - Amount"] = count_amount_df["Percentage Change - Amount"].round(2)
+count_amount_df = count_amount_df.round(2)
+
+#   Ranks - 2016 and 2018
+count_amount_df["2016 Rank"] = count_amount_df["2016 Claim Amount"].rank(ascending=False)
+count_amount_df["2018 Rank"] = count_amount_df["2018 Claim Amount"].rank(ascending=False)
+
+
+
+#   Append the rows of the DataFrame to your worksheet
+count_amount_df.to_csv(r"C:\Users\aridding\Documents\BA Related\Monthly\Scripts\Reports\program_march.csv")
---
+++ BA/monthly_script/script_2_product_table.py
@@ -0,0 +1,75 @@
+#   SCRIPT 2 - By Product Line
+
+#   Using the RAW "LAST DATA PULL" - Groupby Method
+
+#   Import the proper packages
+import pandas as pd
+
+#   Set up the source file path and name
+file_path = r"C:\\Users\\aridding\\Documents\\BA Related\\Monthly\\Monthly Report Development\\"
+_name = "Last Raw Data Pull-AR.csv"
+
+#   Read in the raw file - Check for dataset length (data validation)
+raw_all_years = pd.read_csv(file_path + _name, low_memory = False, encoding='latin-1', index_col=False)
+
+
+#   Split the dataset into 2016 and 2018 years
+all_years_programs_models_2016 = raw_all_years[raw_all_years["sale.year"] == 2016]
+all_years_programs_models_2018 = raw_all_years[raw_all_years["sale.year"] == 2018]
+
+
+#   Print descriptions - COUNT IS NOT ACCURATE FOR 2018 - SUM QUANTITY
+claim_count_2018 = all_years_programs_models_2018.groupby(["product.line"])["quantity"].sum()
+claim_count_2016 = all_years_programs_models_2016.groupby(["product.line"])["quantity"].sum()
+
+
+#   Claim Count - Breakdown by program - DataFrame. 
+df_claim_count_2016 = pd.DataFrame(claim_count_2016)
+df_claim_count_2018 = pd.DataFrame(claim_count_2018)
+
+#   Merge the dataframes to compare across years
+df_merge = pd.concat([df_claim_count_2016, df_claim_count_2018], axis=1)
+
+#   Rename the columns to be able to divide properly
+df_merge.columns = ["2016 Claim Count", "2018 Claim Count"]
+
+#   Percentage Change Calculation
+df_merge["Percentage Change"] = (df_merge["2018 Claim Count"]/df_merge["2016 Claim Count"] *100)- 100
+df_merge["Percentage Change"] = df_merge["Percentage Change"].round(2)
+
+
+#   Print descriptions - COUNT IS NOT ACCURATE FOR 2018 - SUM QUANTITY
+claim_amount_2018 = all_years_programs_models_2018.groupby(["product.line"])["claim.amount"].sum()
+claim_amount_2016 = all_years_programs_models_2016.groupby(["product.line"])["claim.amount"].sum()
+
+
+#   Claim Count - Breakdown by program - DataFrame. 
+df_claim_amount_2016 = pd.DataFrame(claim_amount_2016)
+df_claim_amount_2018 = pd.DataFrame(claim_amount_2018)
+
+
+#   Merge the dataframes to compare across years
+df_merge_amount = pd.concat([df_claim_amount_2016, df_claim_amount_2018], axis=1)
+
+#   Rename the columns to be able to divide properly
+df_merge_amount.columns = ["2016 Claim Amount", "2018 Claim Amount"]
+
+#   Merge the Count and Amount DFs
+count_amount_df = pd.concat([df_merge, df_merge_amount], 1)
+
+#   Percentage Change Calculation
+count_amount_df["Percentage Change - Amount"] = (count_amount_df["2018 Claim Amount"]/count_amount_df["2016 Claim Amount"] *100)- 100
+count_amount_df["Percentage Change - Amount"] = count_amount_df["Percentage Change - Amount"].round(2)
+
+#   Sort Values by Claim amount column
+count_amount_df = count_amount_df.sort_values(by=["2018 Claim Amount"], ascending=False)
+count_amount_df = count_amount_df.round(2)
+
+#   Ranks - 2016 and 2018
+count_amount_df["2016 Rank"] = count_amount_df["2016 Claim Amount"].rank(ascending=False)
+count_amount_df["2018 Rank"] = count_amount_df["2018 Claim Amount"].rank(ascending=False)
+
+
+count_amount_df = count_amount_df.iloc[:9, :]
+
+count_amount_df.to_csv(r"C:\Users\aridding\Documents\BA Related\Monthly\Scripts\Reports\product_march.csv")
